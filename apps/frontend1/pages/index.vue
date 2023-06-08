a0d2e95fa45c9e3e42a2962625a89e07e970a53b
Farhan Israq <farhan.israq.bd@gmail.com>
2023-06-09 04:12:28 +0600

    wip: texts

--- apps/frontend1/pages/index.vue
+++ apps/frontend1/pages/index.vue
@@ -16,11 +16,12 @@
           rel="noopener noreferrer"
           class="text-gray-100 underline"
           href="https://linkedin.com/in/farhanshares"
-          >Farhan Israq</a
-        >, a developer living in Dinajpur, Bangladesh. I currently still work
-        remotely, and also am not actively searching for a job. However, when I
-        came across this job posting, I couldn't resist applying because it
-        truly feels like a dream job — an incredible opportunity that aligns
+        >
+          Farhan Israq</a
+        >, a 25 y/o developer living in Dinajpur, Bangladesh. I currently still
+        work remotely, and also am not actively searching for a job. However,
+        when I came across this job posting, I couldn't resist applying because
+        it truly feels like a dream job — an incredible opportunity that aligns
         perfectly with my aspirations and skills. That being said, let's get to
         my story and why I think we are are a great fit for each other.
       </p>
@@ -28,23 +29,82 @@
       <p>
         Imagine this: back in 2012, as a teenager, I created my Facebook account
         out of pure curiosity. Little did I know that this simple act would
-        ignite my passion for coding. I wondered and Googled how Facebook and
-        Websites were created! I embarked on a self-guided journey, learning
-        PHP, MySQL, HTML, and CSS in my 10th grade. What began as a hobby
-        transformed into a relentless pursuit of knowledge and problem-solving
-        skills. Today, as I venture into the world of veterinary science, I
-        bring a unique blend of coding expertise and analytical thinking.
+        ignite my passion for coding. I wondered, and Googled, "How Facebook and
+        other Websites were created!" I embarked on a self-guided journey,
+        learning PHP, HTML, CSS and a bit of MySQL in my 10th grade. What began
+        as a hobby transformed into a relentless pursuit of knowledge and
+        problem-solving skills. Today, as I'm graduating as a veterinary doctor.
       </p>
-      <h3 class="text-lg font-medium leading-tight">Psst! Why Veterinary?</h3>
+
+      <h3 class="text-lg font-medium leading-tight">Why Life Science?</h3>
       <p>
-        I was loving every moment of my self-taught journey. It was a world
-        brimming with creativity and the freedom to choose my own path. No rigid
-        syllabus held me back. I decided to delve into the fascinating realm of
-        bio-science, driven purely by my passion for exploration. Back then,
-        making money was never my focus; it was all about following my heart's
-        desire for knowledge and pure enjoyment.
+        I was loving every moment of my self-taught journey. No rigid
+        tech-syllabus held me back. I decided to delve into the fascinating
+        realm of life-science, driven purely by my passion for exploration. Back
+        then, making money was never my focus; it was all about following my
+        heart's desire for knowledge and pure enjoyment.
       </p>
-      sf2
+
+      <h3 class="text-lg font-medium leading-tight">How it went?</h3>
+      <p>
+        Initially, I made a whole lot of trail & error, exploring to & fro and
+        sweating for the simplest bug. The more I spent my times, eventually the
+        more it started to become clearer. I used to code in
+        <code>Raw PHP</code>, then <code>Composer</code> came in,
+        <code>Laravel</code> was highly popular in PHP's world. I started my
+        journey with it. Lately, I've created an open source Laravel Package
+        too, called
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-gray-100 underline"
+          href="https://github.com/farhanshares/laravel-mediaman"
+        >
+          Laravel MediaMan</a
+        >. However, <code>Javascript</code> soon became (and still it is) the
+        trend, it can do all the fancy stuffs and everyone was adopting it
+        quickly, so did I. Initially, I started with <code>ReactJS</code> then
+        <code>VueJS</code>. I got hired for mid-level to lead positions as a
+        Frontend Developer Role (VueJS) at different organizations from local
+        and beyond border, counting 3+ years.
+      </p>
+
+      <h3 class="text-lg font-medium leading-tight">Hmm, anything else?</h3>
+      <p>
+        Tech is something where I find joy, I've spent times on learning the
+        principles like a CS graduate would do. Operating System, Computer
+        Networking, Filesystem, Database, DSA, Coding Patterns (e.g. Proxy,
+        Builder, Facade), Principles (e.g. DRY, SOLID, KISS, YAGNI) — I've tried
+        my best to get a good grip in it. I often do problem solving and lately
+        started leetcoding to sharpen my logical thinking.
+      </p>
+
+      <h3 class="text-lg font-medium leading-tight">After Hours</h3>
+      <ul>
+        <li>
+          I also try and keep learning, for example I am learning Python, and,
+          recently learnt Dart / Flutter -
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            class="underline"
+            href="https://play.google.com/store/apps/details?id=com.deenibondhon.app"
+          >
+            My App in Play Store
+          </a>
+        </li>
+        <li>I'm trying to contribute to open source and build up my network</li>
+        <li>
+          I love reading or listening to books or audio-books and podcasts
+        </li>
+      </ul>
+
+      <hr class="my-10 border-t border-gray-200" />
+      <p>
+        There is a bunch more I would love to talk about, but I hope I've
+        already convinced you <em>enough</em> to start a conversation. I am
+        looking forward to your response / feedback!
+      </p>
     </div>
   </div>
 </template>
