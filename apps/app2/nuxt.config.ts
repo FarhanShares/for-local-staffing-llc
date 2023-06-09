41cb23f541fc58f688208871ef79ed079a55d36e
Farhan Israq <farhan.israq.bd@gmail.com>
2023-06-09 11:52:54 +0600

    refactor: rename

--- apps/app2/nuxt.config.ts
+++ apps/app2/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['frontend-module', '@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: ['farhans-module', '@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
   devServer: {
     https: false,
   },
